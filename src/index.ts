5c20f70f1655fa9a20c087e80af3e49b4be72e43
bananahexagon <obsidian.bananafool@gmail.com>
2023-07-22 22:18:42 +0900

    moved library

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 window.onload = async () => {
     let Core =  require("./core");
-    const cLibGen = require("./canvas_lib.ts");
+    const cLibGen = require("./lib/canvas.ts");
     await Core.init();
-    const cLib = cLibGen(Core.ctx, Core.Images)
+    const cLib = cLibGen(Core.ctx, Core.Images);
     cLib.stamp("bananahexagon", 60, 160);
 }
